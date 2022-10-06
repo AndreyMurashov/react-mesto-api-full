07ba5f5b7a20d17d519ea1f7f6d87da23bdbeb6f
Andrey Murashov <amourashov@yandex.ru>
2022-10-06 14:23:38 +0300

    проектная работа 15 начало

--- backend/app.js
+++ backend/app.js
@@ -50,7 +50,7 @@ app.use((err, req, res, next) => {
 });
 
 mongoose
-  .connect('mongodb://localhost:27017/mestodb', {
+  .connect('mongodb://murashov.students.nomoredomains.icu:27017/mestodb', {
     useNewUrlParser: true,
   })
   .then(console.log('DB OK'))
