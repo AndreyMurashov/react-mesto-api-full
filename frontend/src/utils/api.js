a506998e7c83e6b8c5668586119d0910b487db50
Andrey Murashov <amourashov@yandex.ru>
2022-10-07 16:54:52 +0300

    проектная работа 15 подготовлена к ревью

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -78,7 +78,7 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: "api.murashov.students.nomoredomains.icu",
+  baseUrl: "http://api.murashov.students.nomoredomains.icu",
   headers: {
     "Content-Type": "application/json",
   },
