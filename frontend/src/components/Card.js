23d7c8974d4ba1e15fc7c85a002b938375b6439c
AndreyMurashov <amourashov@yandex.ru>
2022-10-07 23:30:50 +0300

    проектная работа 15 перед  ревью

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,7 +5,7 @@ const Card = (props) => {
   const currentUser = useContext(CurrentUserContext);
   const isOwn = props.card.owner === currentUser._id;
   const cardDeleteButtonClassName = "element__thrash";
-  const isLiked = props.card.likes.some((i) => i._id === currentUser._id);
+  const isLiked = props.card.likes.some((i) => i === currentUser._id);
   const cardLikeButtonClassName = `element__like ${
     isLiked ? "element__like_liked" : ""
   }`;
