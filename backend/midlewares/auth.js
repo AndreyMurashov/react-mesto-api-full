55b557e055d484d9a3b77318463139f345b68ef7
Andrey Murashov <amourashov@yandex.ru>
2022-10-11 09:31:13 +0300

    проектная работа 15 после ревью

--- backend/midlewares/auth.js
+++ backend/midlewares/auth.js
@@ -6,6 +6,7 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
   // const { authorization } = req.headers;
   const authorization = req.cookies.jwtToken;
+  console.log(authorization);
   if (!authorization || !authorization.startsWith('Bearer')) {
     return next(new LoginError('Необходима авторизация'));
   }
